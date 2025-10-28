ad71b0db63e285f5ee2bbb2196a2d3275f0cbc6a
elbisacoderbyte <lengaeldad@gmail.com>
2025-10-28 16:55:56 +0100

    feat: resolve exercice compteArebours.js

--- 07-compteARebours.js
+++ 07-compteARebours.js
@@ -1,19 +1,19 @@
 /*
-  /*
-    Énoncé :
-    Écris une fonction `compteARebours()` qui affiche dans la console les nombres de 10 à 1,
-    un nombre par ligne.
+  Énoncé :
+  Écris une fonction `compteARebours()` qui affiche dans la console les nombres de 10 à 1,
+  un nombre par ligne.
 
-    Signature attendue :
-      function compteARebours() -> void
+  Signature attendue :
+    function compteARebours() -> void
+*/
 
-    Placeholder : version neutre (ne log rien) pour permettre l'exécution des tests
-    sans lever d'exception. L'étudiant doit remplacer la fonction par son implémentation.
-  */
-  function compteARebours() {
-    // Placeholder neutre : ne fait rien
+function compteARebours() {
+  // Vérifie qu'on travaille bien avec une boucle et pas d'entrée externe
+  // On part de 10 et on décrémente jusqu'à 1
+  for (let i = 10; i >= 1; i--) {
+    console.log(i);
   }
+}
 
-  // Ne pas modifier la ligne ci-dessous
-  module.exports = { compteARebours }
 // Ne pas modifier la ligne ci-dessous
+module.exports = { compteARebours };
